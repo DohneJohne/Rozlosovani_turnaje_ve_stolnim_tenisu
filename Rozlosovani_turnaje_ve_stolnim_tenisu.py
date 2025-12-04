61d8d2532c25d13c9f4dc3e78fccff262ee1802e
“[dan]” <[djjanovsky@gmail.com]>
2025-12-04 20:58:51 +0100

    Přidána funkční metoda na rozlosování turnaje + přidána funkční metoda na grafické zobrazení turnaje (generování Excelu) + různé změny předchozích metod pro funkčnost kódu

--- Rozlosovani_turnaje_ve_stolnim_tenisu.py
+++ Rozlosovani_turnaje_ve_stolnim_tenisu.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import random
+import openpyxl
+from openpyxl.styles import Font, Alignment
+from openpyxl import Workbook
 
 class Osoba:
     def __init__(self, jmeno, prijmeni):
@@ -14,11 +17,12 @@ class Hrac(Osoba): #Hráč dědí z Osoby, reprezentuje každého hráče turnaj
         self.gender = gender
         self.klub = klub
         self.nasazeni = nasazeni
+        self.kos = None
     def __repr__(self):
         return f"Hrac ({self.jmeno}, {self.prijmeni}, {self.klub})"
     
     def nacist_hrace(soubor): #Python přečte Excel a přiradí k jednotlivým proměnným vlastnosti hráčů dle Excelu
-        df = pd.read_excel(soubor, header=2)
+        df = pd.read_excel(soubor, header=1)
         hraci = []
         for _, row in df.iterrows():
             hrac = Hrac(jmeno = row["Jméno"],
@@ -37,7 +41,7 @@ class Hrac(Osoba): #Hráč dědí z Osoby, reprezentuje každého hráče turnaj
         for hrac in hraci: #Rozřazení probíhá podle toho, co je napsané v kolonce Gender u každého hráče
             if hrac.gender.lower() in ["m", "muž"]: #U každého genderu více možností pro univerzálnost kódu
                 muzi.append(hrac)
-            elif hrac.gender.lower in ["f", "ž", "žena"]:#Zároveň gender převeden na malá písmena, znovu pro zvýšení univerzálnosti
+            elif hrac.gender.lower() in ["f", "ž", "žena"]:#Zároveň gender převeden na malá písmena, znovu pro zvýšení univerzálnosti
                 zeny.append(hrac)
         return muzi, zeny
     
@@ -47,27 +51,29 @@ class Hrac(Osoba): #Hráč dědí z Osoby, reprezentuje každého hráče turnaj
             hrac.nasazeni = index
         return serazeni_hraci
     
-    def zakladni_rozlosovani_na_podskupinky(serazeni_hraci, pocet_skupin):#Metoda, která rozdělí hráče do podskupinek dle jeho nasazení a počtu skupin
-        jednicky =[]
-        dvojky = []
-        trojky = []
-        ctyrky = []
-        petky = []
-        sestky = []
+    def rozdeleni_na_kose(serazeni_hraci, pocet_skupin):#Metoda, která rozdělí hráče do podskupinek dle jeho nasazení a počtu skupin
         delic = pocet_skupin # Proměnná, která rozděluje hráče na podskupinky a dělí tyto podskupinky (12. hráč je jednička, 13. dvojka) 
 #př.: Pokud přijede 48 lidí a budu mít tedy 12 skupin, tak 12 hráčů budou jedničky, 12 dvojky atd... dle jejich nasazení
-        for i in serazeni_hraci[0:delic+1]:
-            jednicky.append(i)
-        for i in serazeni_hraci[delic:(delic)*2+1]:
-            dvojky.append(i)
-        for i in serazeni_hraci[delic*2: (delic*3)+1]:
-            trojky.append(i)
-        for i in serazeni_hraci[delic*3: (delic*4)+1]:
-            ctyrky.append(i)
-        for i in serazeni_hraci[delic*4: (delic*5)+1]:
-            petky.append(i)
-        for i in serazeni_hraci[delic*5: (delic*6)+1]:
-            sestky.append(i)
+        jednicky = serazeni_hraci[0:delic]
+        dvojky = serazeni_hraci[delic:(delic)*2]
+        trojky = serazeni_hraci[delic*2: (delic*3)]
+        ctyrky = serazeni_hraci[delic*3: (delic*4)]
+        petky = serazeni_hraci[delic*4: (delic*5)]
+        sestky = serazeni_hraci[delic*5: (delic*6)]
+        #Každému hráči je přiděleno identifikační číslo, podle toho v jakém koši se nachází
+        for hrac in jednicky:
+            hrac.kos = 1
+        for hrac in dvojky:
+            hrac.kos = 2
+        for hrac in trojky:
+            hrac.kos = 3
+        for hrac in ctyrky:
+            hrac.kos = 4
+        for hrac in petky:
+            hrac.kos = 5
+        for hrac in sestky:
+            hrac.kos = 6
+
         return jednicky, dvojky, trojky, ctyrky, petky, sestky
         
 class Skupina: #Třída, která reprezentuje samotnou Skupinu turnaje
@@ -113,31 +119,129 @@ class Turnaj: #Třída reprezentující samotný turnaj, turnaj má svoje hráč
                     skupina.hraci.append(hrac)
                     break
     
-    def rozlosovani_skupin(self, podskupinky_ze_zakladniho_rozlosovani): #Metoda pro rozlosování zbytku hráčů turnaje
-        while len(podskupinky_ze_zakladniho_rozlosovani) > 0: #Pokud v podskupince je více než 0 hráčů
-            hrac = random.choice(podskupinky_ze_zakladniho_rozlosovani) #Vybere náhodného hráče z podskupinky
+    def rozlosovani_skupin(self, kos): #Metoda pro rozlosování zbytku hráčů turnaje
+        while len(kos) > 0: #Pokud v podskupince je více než 0 hráčů
+            hrac = random.choice(kos) #Vybere náhodného hráče z podskupinky
 
             #Kontrola toho, aby nebyli dva hráči ze stejného klubu v jedné skupině
             vhodne_skupiny = []
             for skupina in self.skupiny:
-               stejny_klub_nalezen = False
-               for hrac_ve_skupine in skupina.hraci: #Podmínka pro to, když se najde duplicita klubů
-                if hrac_ve_skupine.klub == hrac.klub:
-                    stejny_klub_nalezen = True
-            
-            if stejny_klub_nalezen == False: #Pokud vybraný hráč nemá stejný klub, přidá se daná skupina do listu vhodných skupin
+                stejny_klub_nalezen = False
+                stejny_kos_nalezen = False
+                #Pokud se najde duplicitní klub nebo identifikace koše, tak se přeruší for cyklus a prohledává se další skupina
+                for hrac_ve_skupine in skupina.hraci:
+                    if hrac_ve_skupine.klub == hrac.klub:
+                        stejny_klub_nalezen = True
+                        break
+                    elif hrac_ve_skupine.kos == hrac.kos:
+                        stejny_kos_nalezen = True
+                        break
+
+                if not stejny_klub_nalezen and not stejny_kos_nalezen: #Pokud vybraný hráč nemá stejný klub, přidá se daná skupina do listu vhodných skupin
                     vhodne_skupiny.append(skupina)
 
             if len(vhodne_skupiny)>0: #Pokud je alespoň jedna vhodná skupina pro hráče, je tento hráč do té skupiny přidán
-                cilova_skupina = vhodne_skupiny[0]
-                cilova_skupina.hraci.append(hrac)
+                min_pocet = min(len(skupina.hraci) for skupina in vhodne_skupiny) #Najde nejmenší počet hráčů
+                
+                kandidati = []
+                for skupina in vhodne_skupiny:
+                    if len(skupina.hraci) == min_pocet:
+                        kandidati.append(skupina)#Pokud se počet hráčů ve vhodné skupině rovná minimu, skupina se přidá do kandidátů
+                cilova_skupina = random.choice(kandidati) #Náhodně vybere kandidátní skupinu
             else: #Pokud není vhodná skupina, přidá se hráč do skupiny s nejmenším počtem hráčů (nutné porušení pravidla o duplicitě klubů)
-                min_pocet = min(len(s.hraci) for s in self.skupiny) #Při rovnosti hráčů ve více skupinách, program vybere náhodnou skupinu
-                nejlepsi_skupiny = [s for s in self.skupiny if len(s.hraci) == min_pocet]
-                cilova_skupina = random.choice(nejlepsi_skupiny)
-        
-            podskupinky_ze_zakladniho_rozlosovani.remove(hrac)
-#KOMENTÁŘ PRO ÚPRAVU PŘEDCHOZÍHO COMMITU (BUDE SMAZÁNO)
+                min_pocet = min(len(s.hraci) for s in self.skupiny) #Tady se vybírá se minimum ze všech skupin
+                nejlepsi_skupiny = []
+                for skupina in self.skupiny:
+                    if len(skupina.hraci) == min_pocet:
+                        nejlepsi_skupiny.append(skupina)#Pokud se počet hráčů v obecné skupině rovná minimu, skupina se přidá do nejlepsi_skupiny
+                cilova_skupina = random.choice(nejlepsi_skupiny) #Zajištění rovnoměrného rozložení hráčů, náhodně se vybere skupina z nejlepších
+            
+            cilova_skupina.hraci.append(hrac)
+            kos.remove(hrac)
+    
+    def export_turnaje_do_excelu(self, vystup, skupin_na_list):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = f"Skupiny 1-{skupin_na_list}"
+        #Pomocné konstanty pro pozicování
+        mezera_radky = 2
+        aktualni_radek = 1
+        aktualni_sloupec = 1
+        #Název skupiny nad tabulkou skupiny
+        for index_skupiny, skupina in enumerate(self.skupiny, start=1):
+            ws.cell(row=aktualni_radek, column=aktualni_sloupec, 
+                    value=f"Skupina {skupina.id_skupiny}").font= Font(bold=True, size=14)
+            
+            zacatek_dat_row = aktualni_radek + 2
+            zacatek_dat_collumn = aktualni_sloupec
+
+            hraci = skupina.hraci
+            n = len(hraci)
+            #Příkaz pro vyplnění jmen a klubu v prním sloupci
+            for i, h in enumerate(hraci):
+                text = f"{h.jmeno} {h.prijmeni} ({h.nasazeni})\n{h.klub}"
+                bunka = ws.cell(row= zacatek_dat_row + i, column = zacatek_dat_collumn, value=text)
+                bunka.alignment = Alignment(wrap_text=True, vertical="top")
+
+            
+            tabulka_zacatek_sloupec = zacatek_dat_collumn + 1
+            #Hlavička hráčů
+            for i in range (n):
+                ws.cell(row=zacatek_dat_row -1, column= tabulka_zacatek_sloupec + i,
+                        value=f"H{i+1}").font = Font(bold=True)
+            #Vytvoření čtvercové tabulky s délkou strany o počtu hráčů ve skupině + X na diagonále
+            for i in range(n):
+                for j in range(n):
+                    if i==j:
+                        ws.cell(row=zacatek_dat_row + i, column= tabulka_zacatek_sloupec + j,
+                                value = "X").font = Font(bold=True)
+                    else:
+                        ws.cell(row=zacatek_dat_row + i, column= tabulka_zacatek_sloupec + j,
+                                value = "")
+            #Přidání dvou sloupců "Skóre" a "Pořadí" na konec tabulky
+            sloupec_skore = tabulka_zacatek_sloupec + n
+            sloupec_poradi = tabulka_zacatek_sloupec + n + 1
+
+            ws.cell(row= zacatek_dat_row - 1, column= sloupec_skore,
+                    value = "Skóre").font = Font(bold=True)
+            ws.cell(row= zacatek_dat_row - 1, column= sloupec_poradi,
+                    value = "Pořadí").font = Font(bold=True)
+            
+            #Posun na další skupinu pod tu současnou
+            aktualni_radek += n + 1 + mezera_radky
+
+            #Vytvoření nového sheetu pokud dosáhneme pozadovaneho poctu skupin na jednom sheetu
+            if index_skupiny % skupin_na_list == 0:
+            #Zároveň se nastavuje i šířka buňky pro to, aby se tam vešly všechny texty
+                for sloupec in ws.columns:
+                    maximalni_delka = 0
+                    for bunka in sloupec:
+                        if bunka.value:
+                            delka_textu = len(str(bunka.value))
+                            if delka_textu > maximalni_delka:
+                                maximalni_delka = delka_textu
+                    pismeno = sloupec[0].column_letter
+                    ws.column_dimensions[pismeno].width = maximalni_delka + 4
+                dalsi = index_skupiny + 1
+                ws = wb.create_sheet(title= f"Skupiny {dalsi}-{dalsi + skupin_na_list-1}")
+                aktualni_radek = 1
+                aktualni_sloupec = 1
+        #Fitting slov pro poslední sheet (poslední sheet není pod podmínkou if)
+        for sloupec in ws.columns:
+                    maximalni_delka = 0
+                    for bunka in sloupec:
+                        if bunka.value:
+                            delka_textu = len(str(bunka.value))
+                            if delka_textu > maximalni_delka:
+                                maximalni_delka = delka_textu
+                    pismeno = sloupec[0].column_letter
+                    ws.column_dimensions[pismeno].width = maximalni_delka + 4
+        wb.save(vystup)
+        return print(f"Turnaj rozlosován! Vygenerovaný Excel je ve stejné složce, jako tento kód")
+
+
+
+
 #Volání tříd a metod pro správné nalosování turnaje
 hraci = Hrac.nacist_hrace("prihlaseni_hraci_test.xlsx")
 muzi, zeny = Hrac.rozradit_gender(hraci)
@@ -151,8 +255,8 @@ turnaj_muzi.vytvor_skupiny(pocet_hracu_muzi)
 turnaj_zeny.vytvor_skupiny(pocet_hracu_zeny)
 pocet_skupin_muzi = turnaj_muzi.pocet_skupin
 pocet_skupin_zeny = turnaj_zeny.pocet_skupin
-jednicky_muzi, dvojky_muzi, trojky_muzi, ctyrky_muzi, petky_muzi, sestky_muzi =Hrac.zakladni_rozlosovani_na_podskupinky(muzi, pocet_skupin_muzi)
-jednicky_zeny, dvojky_zeny, trojky_zeny, ctyrky_zeny, petky_zeny, sestky_zeny =Hrac.zakladni_rozlosovani_na_podskupinky(zeny, pocet_skupin_zeny)
+jednicky_muzi, dvojky_muzi, trojky_muzi, ctyrky_muzi, petky_muzi, sestky_muzi =Hrac.rozdeleni_na_kose(muzi, pocet_skupin_muzi)
+jednicky_zeny, dvojky_zeny, trojky_zeny, ctyrky_zeny, petky_zeny, sestky_zeny =Hrac.rozdeleni_na_kose(zeny, pocet_skupin_zeny)
 turnaj_muzi.rozlosovani_jednicek(jednicky_muzi)
 turnaj_zeny.rozlosovani_jednicek(jednicky_zeny)
 turnaj_muzi.rozlosovani_skupin(dvojky_muzi)
@@ -165,4 +269,5 @@ turnaj_zeny.rozlosovani_skupin(trojky_zeny)
 turnaj_zeny.rozlosovani_skupin(ctyrky_zeny)
 turnaj_zeny.rozlosovani_skupin(petky_zeny)
 turnaj_zeny.rozlosovani_skupin(sestky_zeny)
-
+turnaj_muzi.export_turnaje_do_excelu("muzi_skupiny.xlsx", skupin_na_list=4)
+turnaj_zeny.export_turnaje_do_excelu("zeny_skupiny.xlsx", skupin_na_list=4)
