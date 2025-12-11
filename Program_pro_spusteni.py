e2accfadc86c0f411e75bd8aec9b8f5c8f8c758e
“[dan]” <[djjanovsky@gmail.com]>
2025-12-11 20:26:56 +0100

    Přidány volání metod týkajících se sedmého koše

--- Program_pro_spusteni.py
+++ Program_pro_spusteni.py
@@ -30,8 +30,8 @@ pocet_skupin_muzi = turnaj_muzi.pocet_skupin
 pocet_skupin_zeny = turnaj_zeny.pocet_skupin
 
 #Rozdělení hráčů do košů (jedničky skupin, dvojky skupin...)
-jednicky_muzi, dvojky_muzi, trojky_muzi, ctyrky_muzi, petky_muzi, sestky_muzi =Hrac.rozdeleni_na_kose(muzi, pocet_skupin_muzi)
-jednicky_zeny, dvojky_zeny, trojky_zeny, ctyrky_zeny, petky_zeny, sestky_zeny =Hrac.rozdeleni_na_kose(zeny, pocet_skupin_zeny)
+jednicky_muzi, dvojky_muzi, trojky_muzi, ctyrky_muzi, petky_muzi, sestky_muzi, sedmicky_muzi =Hrac.rozdeleni_na_kose(muzi, pocet_skupin_muzi)
+jednicky_zeny, dvojky_zeny, trojky_zeny, ctyrky_zeny, petky_zeny, sestky_zeny, sedmicky_zeny =Hrac.rozdeleni_na_kose(zeny, pocet_skupin_zeny)
 
 #Rozlosování všech hráčů do skupin
 turnaj_muzi.rozlosovani_jednicek(jednicky_muzi)
@@ -41,11 +41,13 @@ turnaj_muzi.rozlosovani_skupin(trojky_muzi)
 turnaj_muzi.rozlosovani_skupin(ctyrky_muzi)
 turnaj_muzi.rozlosovani_skupin(petky_muzi)
 turnaj_muzi.rozlosovani_skupin(sestky_muzi)
+turnaj_muzi.rozlosovani_skupin(sedmicky_muzi)
 turnaj_zeny.rozlosovani_skupin(dvojky_zeny)
 turnaj_zeny.rozlosovani_skupin(trojky_zeny)
 turnaj_zeny.rozlosovani_skupin(ctyrky_zeny)
 turnaj_zeny.rozlosovani_skupin(petky_zeny)
 turnaj_zeny.rozlosovani_skupin(sestky_zeny)
+turnaj_zeny.rozlosovani_skupin(sedmicky_zeny)
 
 #Export skupin do Excelu ("<název vašeho turnaje>.xlsx")
 turnaj_muzi.export_turnaje_do_excelu("muzi_skupiny.xlsx", skupin_na_list=4)
